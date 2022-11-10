5cc5f37241ede56f58934e645d54b578e6a815f3
Enmazr <Enmazr@LAPTOP-S2UGQUM2>
2022-11-10 19:52:53 +0700

    Du_An_1

---
+++ src/du_an_1/Du_An_1.java
@@ -0,0 +1,16 @@
+package du_an_1;
+
+/**
+ *
+ * @author Enmazr
+ */
+public class Du_An_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
