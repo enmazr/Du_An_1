3286fb8d4c62f18c65796e005ce21ab9eabe130d
Enmazr <Enmazr@LAPTOP-S2UGQUM2>
2022-11-10 22:06:14 +0700

    hudsh

--- src/du_an_1/Du_An_1.java
+++ src/du_an_1/Du_An_1.java
@@ -11,6 +11,8 @@ public class Du_An_1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello");dsyftydstgf
+
     }
     
 }
